559926ccec1fcd7156694bad2515929ee2309884
nisrinasals <nisrinahikmawan@gmail.com>
2024-06-14 14:23:16 +0700

    membuat void inorder

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -70,3 +70,18 @@ public:
         }
     }
 
+    void inorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
